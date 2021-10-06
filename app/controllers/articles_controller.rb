5ea193294cdfc03808f1362f1e5473e077ac2770
Santiago Loyola <santiagoloyola@hotmail.com>
2021-10-05 21:08:30 -0500

    finish section 6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,30 @@
 class ArticlesController < ApplicationController
 
-		before_action :set_article, only: [:show, :edit, :update, :destroy]
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
-    def show
-    end
+  def show
+  end
 
-    def index
-      @articles = Article.paginate(page: params[:page], per_page: 5)
-    end
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
 
-    def new
-		@article = Article.new
-    end
+  def new
+  @article = Article.new
+  end
 
-    def create
-      @article = Article.new(article_params)
-	  @article.user = User.first
-      if @article.save
-				flash[:notice] = "Article was created successfully."
-        redirect_to @article
-			else
-				render 'new'
-			end
+  def create
+    @article = Article.new(article_params)
+  @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
 	def update
 		if @article.update(article_params)
@@ -51,5 +53,12 @@ class ArticlesController < ApplicationController
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :articles
   before_save { self.email = email.downcase }
+  has_many :articles, dependent: :destroy
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: { minimum: 3, maximum: 25 }
